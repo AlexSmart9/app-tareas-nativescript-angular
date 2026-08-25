f863350d54744c239fea36085987738eccaac485
Alejandro <alex247martinez@outlook.es>
2026-08-25 15:31:43 -0600

    feat(details): Implement pull-to-refresh and grid layout for comments

--- src/app/search/items-details/item-details.component.ts
+++ src/app/search/items-details/item-details.component.ts
@@ -9,9 +9,30 @@ import { RouterExtensions } from '@nativescript/angular';
 
 export class ItemDetailsComponent implements OnInit {
 
+  comments: any[] = [
+    {user: 'guest_1', text: "This is a comment", date: new Date()},
+    {user: 'guest_2', text: "This is another comment", date: new Date()},
+  ]
+
   constructor(private routerExtensions: RouterExtensions) {}
 
   ngOnInit(): void {
+  }
+
+  //  Function Pull to Refresh
+
+  onPull( e: any ) {
+    const pullRefresh = e.object;
+
+    setTimeout(() => {
+      this.comments.push({
+        user: 'guest_' + Math.floor(Math.random() * 100),
+        text: "This is a new comment added by pull to refresh",
+        date: new Date()
+      });
+
+      pullRefresh.refreshing = false;
+    })
   }
   
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
-
+import { registerElement } from '@nativescript/angular';
 import { AppModule } from './app/app.module';
+
+registerElement("PullToRefresh", () => require("@nativescript-community/ui-pulltorefresh").PullToRefresh);
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
